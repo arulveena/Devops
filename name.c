a6ffccc0bd4a63002bc641974be81078fc8a91c0
Arul Praveena Boopalagan <96655829+arulveena@users.noreply.github.com>
2022-03-01 12:44:42 +0530

    Create name.c

---
+++ name.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+void main()
+{
+  printf("Hello everyone \n This is Arul Praveena from CSE A.Currently pursuing the graduation in Dr.Mahalingam college of engineering and technology.");
+}
