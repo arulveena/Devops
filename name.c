712b750f555d3fab14f47129d9a9dff605c3e853
roshini22 <100598649+roshini22@users.noreply.github.com>
2022-03-01 12:51:12 +0530

    Update name.c

--- name.c
+++ name.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
-void main()
+int main()
 {
   printf("Hello everyone \n This is Arul Praveena from CSE A.Currently pursuing the graduation in Dr.Mahalingam college of engineering and technology.");
+ return 0;
 }
+
